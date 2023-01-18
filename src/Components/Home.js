2c0c42e0de862616a85a53f086c4cb7682298071
Eli Cohen <cohen.elijahlev@gmail.com>
2023-01-18 16:35:21 -0500

    home page changed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,30 +5,22 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+
+import './Home.css';
+
 const Home = () => {
 	const navigate = useNavigate();
 
 	return (
 		<div className="Home">
-			<Typography variant="h1">Ready for a cocktail?</Typography>
-			<div className="Home-section">
-				<Typography variant="h5">
-					Have ingredients at home but don't know what cocktail you can make with them? Let us help you decide!
-				</Typography>
-
-				<Button variant="outlined" onClick={() => navigate('/ingredients')}>
-					Select your ingredients
-				</Button>
+			<h1>Ready for a cocktail?</h1>
+			<div
+				className="Home-image-container"
+				style={{ backgroundImage: 'url("../../assets/bar-wall.jpg")', backgroundSize: 'cover' }}
+			>
+				<button>Enter my ingredients</button>
+				<button>Recommend a random cocktail</button>
 			</div>
-			{/* <div className="Home-section">
-				<Typography variant="h5">
-					Willing to go out and buy whatever you need for any drink?
-				</Typography>
-
-				<Button variant="outlined" onClick={() => navigate('/ingredients')}>
-					Get a random cocktail
-				</Button>
-			</div> */}
 		</div>
 	);
 };
