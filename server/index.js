4e3dcde6f0618cd77773bcc0730eb730bc0ed70a
Eli Cohen <cohen.elijahlev@gmail.com>
2023-01-18 10:22:16 -0500

    reorganized server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+app.use('/api/drinks', require('./api/drinks'));
+app.use('/api/ingredients', require('./api/ingredients'));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+const initialize = () => {
+	const port = process.env.PORT || 3000;
+	app.listen(port, () => console.log(`app listening on port ${port}`));
+};
+
+initialize();
