8bde3a43580eebe67669629e3f6f26e0dbc3a0d3
Eli Cohen <cohen.elijahlev@gmail.com>
2023-01-18 10:52:47 -0500

    moved drinks call into server

--- server/api/drinks.js
+++ server/api/drinks.js
@@ -1,4 +1,51 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express.Router();
+
+app.get('/', async (req, res, next) => {
+	try {
+		const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php', {
+			params: {
+				c: 'Cocktail',
+			},
+		});
+
+		let drinks = response.data.drinks;
+
+		for (let drink of drinks) {
+			const detailsResponse = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php', {
+				params: {
+					i: drink.idDrink,
+				},
+			});
+
+			for (let property in detailsResponse.data.drinks[0]) {
+				if (detailsResponse.data.drinks[0][property]) {
+					drink[property] = detailsResponse.data.drinks[0][property];
+				}
+			}
+		}
+
+		for (let drink of drinks) {
+			drink.ingredients = [];
+			drink.measurements = [];
+
+			for (let property in drink) {
+				if (property.includes('Ingredient')) {
+					drink.ingredients.push(drink[property]);
+					drink.measurements.push(drink[`strMeasure${property[property.length - 1]}`]);
+
+					delete drink[property];
+					delete drink[`strMeasure${property[property.length - 1]}`];
+				}
+			}
+		}
+
+		res.send(drinks);
+	} catch (ex) {
+		next(ex);
+	}
+});
 
 module.exports = app;
--- src/store/index.js
+++ src/store/index.js
@@ -40,44 +40,9 @@ export const fetchIngredients = () => {
 
 export const fetchDrinks = () => {
 	return async (dispatch) => {
-		const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php', {
-			params: {
-				c: 'Cocktail',
-			},
-		});
+		const response = await axios.get('/api/drinks');
 
-		let drinks = response.data.drinks;
-
-		for (let drink of drinks) {
-			const detailsResponse = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php', {
-				params: {
-					i: drink.idDrink,
-				},
-			});
-
-			for (let property in detailsResponse.data.drinks[0]) {
-				if (detailsResponse.data.drinks[0][property]) {
-					drink[property] = detailsResponse.data.drinks[0][property];
-				}
-			}
-		}
-
-		for (let drink of drinks) {
-			drink.ingredients = [];
-			drink.measurements = [];
-
-			for (let property in drink) {
-				if (property.includes('Ingredient')) {
-					drink.ingredients.push(drink[property]);
-					drink.measurements.push(drink[`strMeasure${property[property.length - 1]}`]);
-
-					delete drink[property];
-					delete drink[`strMeasure${property[property.length - 1]}`];
-				}
-			}
-		}
-
-		dispatch({ type: 'drinks/set', drinks: drinks });
+		dispatch({ type: 'drinks/set', drinks: response.data });
 	};
 };
 
