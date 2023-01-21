d4e891a545ba2bd69545939ea26ae526fa723427
Eli Cohen <cohen.elijahlev@gmail.com>
2023-01-21 09:24:49 -0500

    header menu functioning

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 import './Header.css';
 
 const Header = () => {
+	const navigate = useNavigate();
+
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
 	return (
 		<header>
 			<Link className="header-home" to="/">
 				<HomeRoundedIcon />
 			</Link>
-			<div className="header-menu">Find me a cocktail</div>
+			<div id="header-menu" className="header-menu" onClick={handleClick}>
+				Find me a cocktail
+			</div>
+
+			<Menu
+				id="nav-menu"
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				MenuListProps={{
+					'aria-labelledby': 'header-menu',
+				}}
+			>
+				<Link to="/ingredients" onClick={handleClose} name="ingredients">
+					Enter my ingredients
+				</Link>
+				<Link to="/matches/random" onClick={handleClose} name="random">
+					Recommend a random cocktail
+				</Link>
+			</Menu>
 		</header>
 	);
 };
